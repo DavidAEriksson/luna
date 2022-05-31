e1363a3c8bab1232c3e8fb6595c68259abcac740
David <david.eriksson@valtech.com>
2022-05-31 13:53:30 +0200

    feat: syntax and editor hl groups

--- lua/luna/theme.lua
+++ lua/luna/theme.lua
@@ -2,10 +2,10 @@ local M = {}
 
 -- Loads all highlight groups
 M.highlights = function(colors, config)
-  print("here")
-  local syntax = require('luna.colors.groups.syntax').load(colors, config)
-
-  return syntax
+  local syntax = require('luna.colors.groups.syntax').load_syn(colors, config)
+  local editor = require('luna.colors.groups.editor').load_ed(colors, config)
+  return vim.tbl_deep_extend("error", syntax, editor)
 end
 
 return M
+
