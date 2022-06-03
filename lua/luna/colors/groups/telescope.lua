fc99e4eddd7c1346c1b86d39a61e97adb59ca552
David <david.eriksson@valtech.com>
2022-06-03 11:43:04 +0200

    feat(telescope): use colors from luna

--- lua/luna/colors/groups/telescope.lua
+++ lua/luna/colors/groups/telescope.lua
@@ -9,7 +9,7 @@ M.load_telescope = function(colors)
     TelescopePromptPrefix = { bg = colors.telescope_prompt_prefix },
     TelescopePromptNormal = { bg = colors.telescope_prompt_normal },
     TelescopeResultsNormal = { bg = colors.telescope_results_normal },
-    TelescopePreviewNormal = { bg = colors.telescope_results_normal },
+    TelescopePreviewNormal = { bg = colors.telescope_preview_normal },
 
     TelescopePromptBorder = { bg = colors.telescope_prompt_border, fg = colors.telescope_prompt_border },
     TelescopeResultsBorder = { bg = colors.telescope_results_border, fg = colors.telescope_results_border },
@@ -17,7 +17,7 @@ M.load_telescope = function(colors)
 
     TelescopePromptTitle = { fg = colors.telescope_prompt_title, bg = colors.orange },
     TelescopeResultsTitle = { fg = colors.telescope_results_title },
-    TelescopePreviewTitle = { fg = colors.telescope_preview_title, bg = colors.yellow },
+    TelescopePreviewTitle = { fg = colors.telescope_preview_title, bg = colors.orange},
   }
   return telescope
 end
