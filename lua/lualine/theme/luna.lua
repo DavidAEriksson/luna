21d66b7e13935387567299abb72b2763966340fa
David Eriksson <david.eriksson@valtech.com>
2022-08-24 09:10:44 +0200

    fix: lualine module update

--- lua/lualine/theme/luna.lua
+++ lua/lualine/theme/luna.lua
@@ -1,37 +1,37 @@
 local colors = require("luna.colors").load()
 
-local onenord = {}
+local luna = {}
 
-onenord.normal = {
+luna.normal = {
   a = { fg = colors.gray.void, bg = colors.blue.triton, gui = "bold" },
   b = { fg = colors.blue.triton, bg = colors.gray.gloom },
   c = { fg = colors.white.smoke, bg = colors.gray.dark },
 }
 
-onenord.insert = {
+luna.insert = {
   a = { fg = colors.gray.void, bg = colors.green.terra, gui = "bold" },
   b = { fg = colors.green.terra, bg = colors.gray.gloom },
 }
 
-onenord.command = {
+luna.command = {
   a = { fg = colors.gray.void, bg = colors.yellow.rhea, gui = "bold" },
   b = { fg = colors.yellow.rhea, bg = colors.gray.gloom },
 }
 
-onenord.visual = {
+luna.visual = {
   a = { fg = colors.gray.void, bg = colors.purple.nebula, gui = "bold" },
   b = { fg = colors.purple.nebula, bg = colors.gray.gloom },
 }
 
-onenord.replace = {
+luna.replace = {
   a = { fg = colors.gray.void, bg = colors.red.deimos, gui = "bold" },
   b = { fg = colors.red.deimos, bg = colors.gray.gloom },
 }
 
-onenord.inactive = {
+luna.inactive = {
   a = { fg = colors.gray.smoke, bg = colors.gray.deep, gui = "bold" },
   b = { fg = colors.gray.smoke, bg = colors.gray.deep },
   c = { fg = colors.gray.smoke, bg = colors.gray.dark },
 }
 
-return onenord
+return luna
