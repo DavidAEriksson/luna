ff0772fe8348b59a1072d0e9107a65e952d3efde
DavidAEriksson <DavidAEriksson@users.noreply.github.com>
2022-08-24 06:59:45 +0000

    chore: format with stylua

--- lua/lualine/theme/luna.lua
+++ lua/lualine/theme/luna.lua
@@ -1,35 +1,35 @@
-local colors = require("luna.colors").load()
+local colors = require('luna.colors').load()
 
 local onenord = {}
 
 onenord.normal = {
-  a = { fg = colors.gray.void, bg = colors.blue.triton, gui = "bold" },
+  a = { fg = colors.gray.void, bg = colors.blue.triton, gui = 'bold' },
   b = { fg = colors.blue.triton, bg = colors.gray.gloom },
   c = { fg = colors.white.smoke, bg = colors.gray.dark },
 }
 
 onenord.insert = {
-  a = { fg = colors.gray.void, bg = colors.green.terra, gui = "bold" },
+  a = { fg = colors.gray.void, bg = colors.green.terra, gui = 'bold' },
   b = { fg = colors.green.terra, bg = colors.gray.gloom },
 }
 
 onenord.command = {
-  a = { fg = colors.gray.void, bg = colors.yellow.rhea, gui = "bold" },
+  a = { fg = colors.gray.void, bg = colors.yellow.rhea, gui = 'bold' },
   b = { fg = colors.yellow.rhea, bg = colors.gray.gloom },
 }
 
 onenord.visual = {
-  a = { fg = colors.gray.void, bg = colors.purple.nebula, gui = "bold" },
+  a = { fg = colors.gray.void, bg = colors.purple.nebula, gui = 'bold' },
   b = { fg = colors.purple.nebula, bg = colors.gray.gloom },
 }
 
 onenord.replace = {
-  a = { fg = colors.gray.void, bg = colors.red.deimos, gui = "bold" },
+  a = { fg = colors.gray.void, bg = colors.red.deimos, gui = 'bold' },
   b = { fg = colors.red.deimos, bg = colors.gray.gloom },
 }
 
 onenord.inactive = {
-  a = { fg = colors.gray.smoke, bg = colors.gray.deep, gui = "bold" },
+  a = { fg = colors.gray.smoke, bg = colors.gray.deep, gui = 'bold' },
   b = { fg = colors.gray.smoke, bg = colors.gray.deep },
   c = { fg = colors.gray.smoke, bg = colors.gray.dark },
 }
